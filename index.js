3f0ee844cacfbbafe3ba65b031055a69acedf215
Allen Almario <77124270+allenalmario@users.noreply.github.com>
2022-03-15 05:17:46 +0000

    Change content and added pictures

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = 'My Home Page';
+  res.render('pages/index', {title:title});
+});
+
+app.get('/lakers', (req, res) => {
+  var title = 'Lakers Page';
+  res.render('pages/lakers', {title:title});
+});
+
+app.get('/warriors', (req, res) => {
+  var title = 'Warriors Page';
+  res.render('pages/warriors', {title:title});
+});
+
+app.get('/clippers', (req, res) => {
+  var title = 'Clippers Page';
+  res.render('pages/clippers', {title:title});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
